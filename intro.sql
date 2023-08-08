9bc87e4ffd49977165a7565c8c96e983191030a4
Arman-Kabir <armankabir8@gmail.com>
2023-08-09 00:39:56 +0600

    few modifications

--- intro.sql
+++ intro.sql
@@ -2,40 +2,34 @@
 
 -- ## CREATE DATABASE
 
--- CREATE DATABASE test2;
+CREATE DATABASE test2;
 
 -- ## RENAME DATABASE
 
--- ALTER DATABASE test2 RENAME TO test3;
+ALTER DATABASE test2 RENAME TO test3;
 
 -- ## DELETE DATABASE
 
--- DROP DATABASE test3;
+DROP DATABASE test3;
 
 --## switch to a db (AT MySQL)
 
--- USE test3;
+USE test3;
 
 -- ## Create a student TABLE
 
--- create table
-
---     student(
-
---         student_id INT,
-
---         first_name VARCHAR(50),
-
---         last_name VARCHAR(50),
-
---         cgpa NUMERIC(1, 2)
-
---     );
+create table
+    student(
+        student_id INT,
+        first_name VARCHAR(50),
+        last_name VARCHAR(50),
+        cgpa NUMERIC(1, 2)
+    );
 
 -- ## Rename a table name
 
--- ALTER TABLE student RENAME TO learners;
+ALTER TABLE student RENAME TO learners;
 
 -- ## delete A TABLE
 
--- DROP TABLE learners;
+DROP TABLE learners;
